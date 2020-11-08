2a0261cbc09049ade4d9961b86699146d33e7ada
azadokusluk <azad.okusluk@gmail.com>
2020-11-08 18:38:21 +0300

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using HavaDurumuUygulaması.Models;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HavaDurumuUygulaması.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HavaDurumu()
+        {
+            ViewBag.Message = "Hava Durumu Uygulaması";
+            HavaDurumu h = HavaDurumuGetir();
+
+            return View(h);
+        }
+        public HavaDurumu HavaDurumuGetir()
+        {
+            var client = new RestClient("https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=ankara"); //city= kısmına başka bir şehir yazılabilir.
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("authorization", "apikey 6CnoEMqMXemR5NIZVd13gF:3YOP8OzqTdEHRhyLBtG76c");
+            request.AddHeader("content-type", "application/json");
+            IRestResponse response = client.Execute(request);
+
+            HavaDurumu hava = JsonConvert.DeserializeObject<HavaDurumu>(response.Content);
+
+
+            return hava;
+        }
+
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HavaDurumuUygulaması.MvcApplication" Language="C#" %>
